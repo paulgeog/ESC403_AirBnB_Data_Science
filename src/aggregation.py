87d2c103fe55de986112e09659b6dab566854ebb
Paul Ghisletti <paulursin.ghisletti@uzh.ch>
2026-04-23 13:33:11 +0200

    added: aggregation.py: contains aggregation algorithm to pivot the housing dataset around quartiere (only 34 rows)

---
+++ src/aggregation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def quartiere_standardized_df(df: pd.DataFrame) -> pd.DataFrame:
+    df_q = df.pivot_table(index=["quarlang", "quarsort", "kreissort"],
+                          columns=["eigentuemersszpubl1lang", "anzzimmerlevel2lang_nodm"],
+                          values="anzwhgstat",
+                          aggfunc="sum"
+                          ).reset_index()
+    
+    df_q = df_q.fillna(0)
+
+    # add total housing stock by room number
+    room_types = [
+        "1-Zimmer",
+        "2-Zimmer",
+        "3-Zimmer",
+        "4-Zimmer",
+        "5-Zimmer",
+        "6-Zimmer und mehr"
+        ]
+    for room in room_types:
+        cols = [c for c in df_q.columns if room in c]
+        df_q[f"total_{room}"] = df_q[cols].sum(axis=1)
+
+    # add total housing stock by quartier
+    df_q["total_units"] = df_q[[f"total_{r}" for r in room_types]].sum(axis=1)
+
+    return df_q
