c605ea4eefbd8cb3cb6d0cb6c8941fea3dc645f8
Paul Ghisletti <paulursin.ghisletti@uzh.ch>
2026-04-23 15:11:20 +0200

    aggregation.py: new function for aggregating the rental dataset to quartier level

--- src/aggregation.py
+++ src/aggregation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def quartiere_standardized_df(df: pd.DataFrame) -> pd.DataFrame:
+def quartiere_standardized_housing(df: pd.DataFrame) -> pd.DataFrame:
     df_q = df.pivot_table(index=["quarlang", "quarsort", "kreissort"],
                           columns=["eigentuemersszpubl1lang", "anzzimmerlevel2lang_nodm"],
                           values="anzwhgstat",
@@ -24,5 +24,26 @@ def quartiere_standardized_df(df: pd.DataFrame) -> pd.DataFrame:
 
     # add total housing stock by quartier
     df_q["total_units"] = df_q[[f"total_{r}" for r in room_types]].sum(axis=1)
+    df_q = df_q.reset_index()
+    df_q.index.name = None
 
+    return df_q
+
+
+def quartiere_standardized_rental(df: pd.DataFrame) -> pd.DataFrame:
+    df_q = df.pivot_table(
+        index=["gliederunglang", "gliederungsort"],
+        columns="is_gemeinnuetzig",
+        values="qu50",
+        aggfunc="mean"
+    )
+    df_q = df_q.rename(columns={
+        0: "rent_non_gemeinnuetzig",
+        1: "rent_gemeinnuetzig"
+    })
+    df_q["rent_mean_both"] = df_q[
+        ["rent_non_gemeinnuetzig", "rent_gemeinnuetzig"]
+    ].mean(axis=1)
+    df_q = df_q.reset_index()
+    df_q.index.name = None
     return df_q
